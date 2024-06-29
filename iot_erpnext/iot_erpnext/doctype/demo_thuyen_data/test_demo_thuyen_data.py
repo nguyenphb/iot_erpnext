5deb34a59f046971f02ac1d61e3b6c869703057a
baonguyen2409 <baonguyen2409@gmail.com>
2024-06-29 15:55:48 +0700

    update

---
+++ iot_erpnext/iot_erpnext/doctype/demo_thuyen_data/test_demo_thuyen_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Pyroject and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testdemo_thuyen_data(FrappeTestCase):
+	pass
